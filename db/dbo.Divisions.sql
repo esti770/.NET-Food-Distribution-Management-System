13e053464094513b2f6672b70ba0c8fddad8cf5d
esti_nemirovski <45630158+esti770@users.noreply.github.com>
2021-10-11 02:11:38 +0300

    Add files via upload

---
+++ db/dbo.Divisions.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Divisions] (
+    [codeDiv] INT      IDENTITY (1, 1) NOT NULL,
+    [empId]   INT      NULL,
+    [dateDiv] DATETIME NULL,
+    [divide]  BIT      NULL,
+    CONSTRAINT [PK_dbo.Divisions] PRIMARY KEY CLUSTERED ([codeDiv] ASC)
+);
+
